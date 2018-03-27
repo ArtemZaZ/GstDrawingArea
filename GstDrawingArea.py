bf2ba4fb39560b615be4a2f9172d78518d4b261a
ArtemZaZ <temka.911@mail.ru>
2018-03-27 19:12:45 +0300

    Добавил индикатор ожидания

--- GstDrawingArea.py
+++ GstDrawingArea.py
@@ -2,6 +2,7 @@ import GstCV
 import gi
 import cairo
 import numpy
+import math
 gi.require_version("Gst", "1.0")
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gst, Gtk, GObject, GLib
@@ -9,6 +10,22 @@ from gi.repository import Gst, Gtk, GObject, GLib
 
 def defaultDraw(self, widget, cr):
     pass
+
+
+class WaitIndicator(object):
+    trs = (
+        (0.0, 0.15, 0.30, 0.5, 0.65, 0.80, 0.9, 1.0),
+        (1.0, 0.0, 0.15, 0.30, 0.5, 0.65, 0.8, 0.9),
+        (0.9, 1.0, 0.0, 0.15, 0.3, 0.5, 0.65, 0.8),
+        (0.8, 0.9, 1.0, 0.0, 0.15, 0.3, 0.5, 0.65),
+        (0.65, 0.8, 0.9, 1.0, 0.0, 0.15, 0.3, 0.5),
+        (0.5, 0.65, 0.8, 0.9, 1.0, 0.0, 0.15, 0.3),
+        (0.3, 0.5, 0.65, 0.8, 0.9, 1.0, 0.0, 0.15),
+        (0.15, 0.3, 0.5, 0.65, 0.8, 0.9, 1.0, 0.0,)
+    )
+
+    CLIMIT = 1000
+    NLINES = 8
 
 
 class GstDrawingArea(Gtk.DrawingArea):
@@ -26,6 +43,7 @@ class GstDrawingArea(Gtk.DrawingArea):
         self.img = None     # изображение с альфа-каналом
         self.drawCallBack = drawCallBack    # устанавливаем ф-ию внешней отрисовки
         GLib.timeout_add(10, self.on_timer)
+        self.count = 0
 
     def doDraw(self, widget, cr):       # ф-ия вызывается при отрисовке
         if self.source.cvImage is not None:
@@ -41,10 +59,15 @@ class GstDrawingArea(Gtk.DrawingArea):
             cr.rectangle(0, 0, self.resolution[0], self.resolution[1])
             cr.fill()
             self.drawCallBack(self, width, cr)
-            return False
+        else:
+            self.drawWaitIndicator(cr)
+        return False
 
     def on_timer(self):     # если возвращает True будет рендериться вечно, False - не рендерится
         self.queue_draw()
+        self.count = self.count + 0.1
+        if self.count >= WaitIndicator.CLIMIT:
+            self.count = 0
         return True     #
 
     def doUnrealize(self, arg):
@@ -61,3 +84,16 @@ class GstDrawingArea(Gtk.DrawingArea):
 
     def drawCallBack(self, widget, cr):     # перегружаемая ф-ия для отрисовки
         pass
+
+    def drawWaitIndicator(self, cr):
+        cr.set_line_width(3)
+        cr.set_line_cap(cairo.LINE_CAP_ROUND)
+
+        cr.translate(self.resolution[0] / 2, self.resolution[1] / 2)
+
+        for i in range(WaitIndicator.NLINES):
+            cr.set_source_rgba(0, 0, 0, WaitIndicator.trs[int(self.count) % 8][i])
+            cr.move_to(0.0, -10.0)
+            cr.line_to(0.0, -40.0)
+            cr.rotate(math.pi / 4)
+            cr.stroke()
