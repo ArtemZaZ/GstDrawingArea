80c6fd098ab1c9b4ad0094566f314752ae673338
ArtemZaZ <temka.911@mail.ru>
2018-03-27 16:07:04 +0300

    Убрал лишнее преобразование, убрал из зависимостей PIL, добавил ф-ию внешней отрисовки

--- GstDrawingArea.py
+++ GstDrawingArea.py
@@ -1,45 +1,46 @@
 import GstCV
 import gi
-
+import cairo
+import numpy
 gi.require_version("Gst", "1.0")
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gst, Gtk, GObject, GLib
-import cairo
-from PIL import Image
-import numpy
-import threading
+
+
+def defaultDraw(self, widget, cr):
+    pass
 
 
 class GstDrawingArea(Gtk.DrawingArea):
     # __gsignals__ = {"expose-event": "override", "unrealize": "override"}
 
     def __init__(self, IP="127.0.0.1", RTP_RECV_PORT0=5000, RTCP_RECV_PORT0=5001, RTCP_SEND_PORT0=5005, codec="JPEG",
-                 resolution=[640, 480]):
-        Gtk.DrawingArea.__init__(self)
-        # super(GstDrawingArea, self).__init__(self)  # инициализируем DrawingArea
-        self.resolution = resolution
+                 resolution=[640, 480], drawCallBack=defaultDraw):
+        Gtk.DrawingArea.__init__(self)  # инициализируем родителя
+        self.resolution = resolution    # разрешение
         """видео:"""
-        self.source = GstCV.CVGstreamer(IP, RTP_RECV_PORT0, RTCP_RECV_PORT0, RTCP_SEND_PORT0, codec="JPEG")
-        self.connect("draw", self.doDraw)
-        self.connect("unrealize", self.doUnrealize)
-        self.set_size_request(resolution[0], resolution[1])
-        self.img = None
-        GLib.timeout_add(7, self.on_timer)
+        self.source = GstCV.CVGstreamer(IP, RTP_RECV_PORT0, RTCP_RECV_PORT0, RTCP_SEND_PORT0, codec=codec)
+        self.connect("draw", self.doDraw)   # привязка отрисовки
+        self.connect("unrealize", self.doUnrealize)     # привязка освобождения ресурсов
+        self.set_size_request(resolution[0], resolution[1])     # ставим разрешение на виджет
+        self.img = None     # изображение с альфа-каналом
+        self.drawCallBack = drawCallBack    # устанавливаем ф-ию внешней отрисовки
+        GLib.timeout_add(10, self.on_timer)
 
-    def doDraw(self, widget, cr):
+    def doDraw(self, widget, cr):       # ф-ия вызывается при отрисовке
         if self.source.cvImage is not None:
-            # print(self.source.cvImage)
             height, width, channels = self.source.cvImage.shape
-            # print(self.source.cvImage.flat)
-            self.img = Image.frombytes("RGB", [width, height], self.source.cvImage.flatten(), "raw", "RGB")
-            self.img.putalpha(255)
-            arr = numpy.array(self.img)
-            surface = cairo.ImageSurface.create_for_data(arr, cairo.FORMAT_RGB24, width, height)
+            z = numpy.full((height, width, 1), 255, dtype=numpy.uint8)  # создаем массив 3х3 и заполняем глубину
+            # значением 255
+            self.img = numpy.append(self.source.cvImage, z, axis=2)     # Добавляем к каждому пикселю значение
+            # альфа-канала
+            surface = cairo.ImageSurface.create_for_data(self.img, cairo.FORMAT_RGB24, width, height)
             pt1 = cairo.SurfacePattern(surface)
             pt1.set_extend(cairo.EXTEND_REPEAT)
             cr.set_source(pt1)
             cr.rectangle(0, 0, self.resolution[0], self.resolution[1])
             cr.fill()
+            self.drawCallBack(self, width, cr)
             return False
 
     def on_timer(self):     # если возвращает True будет рендериться вечно, False - не рендерится
@@ -57,3 +58,6 @@ class GstDrawingArea(Gtk.DrawingArea):
 
     def paused(self):
         self.source.paused()
+
+    def drawCallBack(self, widget, cr):     # перегружаемая ф-ия для отрисовки
+        pass
